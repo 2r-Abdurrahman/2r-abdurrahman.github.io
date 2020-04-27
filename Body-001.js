a25197852649f9f3e74c58c5e156bb034f7c41b0
2r-Abdurrahman <62196839+2r-Abdurrahman@users.noreply.github.com>
2020-04-27 16:12:14 +0700

    Add files via upload

---
+++ Body-001.js
@@ -0,0 +1,16 @@
+<body style="text-align:center">
+
+<div id='fixedban' style='width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed:0;z-index:999;-webkit-transform:translateZ(0);'>
+<div><a id='close-fixedban' onclick='document.getElementById(&apos;fixedban&apos;).style.display = &apos;none&apos;;' style='cursor:pointer;'><img alt='close' src='http://2.bp.blogspot.com/-aRTMaSBhxJg/Vjj4aRYEn6I/AAAAAAAAL6E/RzooTTh7ImE/s1600/close-btn.png' style='vertical-align:middle;' title='close button'/></a></div>
+<div style='text-align:center;display:block;max-width:400px;height:auto;overflow:hidden;margin:auto'>
+<a href='https://www.blibli.com/?appsWebview=true&utm_source=propeller_ads&utm_medium=popads_cpc&utm_campaign=homepage_smartcpm&utm_content=3227846' title='Banner Iklan Disini'><img alt='Banner Iklan Disini' src='https://bit.ly/356ltyw' style='max-width:100%;height:auto;vertical-align:middle;'/></a>
+</div>
+</div>
+  
+<script>
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
